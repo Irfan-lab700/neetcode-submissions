34288f77abc269977b9485ab94329ae77ebec2c5
Irfan Khan <irfankhan016104@gmail.com>
2026-07-08 08:51:44 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int helper(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        if (nums.size() == 1) return nums[0];
+
+        vector<int> dp(nums.size());
+
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        return dp.back();
+    }
+
+    int rob(vector<int>& nums) {
+
+        int n = nums.size();
+
+        if (n == 1) return nums[0];
+
+        vector<int> excludeLast(nums.begin(), nums.end() - 1);
+        vector<int> excludeFirst(nums.begin() + 1, nums.end());
+
+        return max(helper(excludeLast), helper(excludeFirst));
+    }
+};
