075332838d3ed2a81a4ed6721e7eac224f21cf2c
Irfan Khan <irfankhan016104@gmail.com>
2026-07-11 08:13:54 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int min = prices[0];
+        int maxprofit = 0;
+        int currprofit = 0;
+        for(int i = 0;i<n;i++){
+            if(prices[i]<min){
+                min = prices[i];
+            }
+            currprofit = prices[i] - min;
+            maxprofit = max(maxprofit,currprofit);
+        }
+        return maxprofit;
+        
+    }
+};
